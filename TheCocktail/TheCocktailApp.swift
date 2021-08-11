e8293e981f504b0f533440c0c691ce5f97292d12
jotkafomat <k-j@xl.wp.pl>
2021-08-11 19:29:49 +0100

    Initial Commit

---
+++ TheCocktail/TheCocktailApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheCocktailApp.swift
+//  TheCocktail
+//
+//  Created by Krzysztof Jankowski on 11/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct TheCocktailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
