5aa50dbbb0970ab0d5a953cc15197250c47878b9
jotkafomat <k-j@xl.wp.pl>
2021-08-24 12:06:22 +0100

    add Recipe model and preview data

---
+++ TheCocktail/Preview Content/CoctailsPreviewData.swift
@@ -0,0 +1,38 @@
+//
+//  CoctailsPreviewData.swift
+//  TheCocktail
+//
+//  Created by Krzysztof Jankowski on 24/08/2021.
+//
+
+import Foundation
+
+let cocktails = [
+    Recipe(
+        id: "food/2020/jun/26/coconut-colada-cocktail-recipe-paradise-soho-sri-lanka",
+        firstPublicationDate: Date(),
+        headline: "Cocktail of the week: Paradise Soho’s coconut colada – recipe",
+        thumbnail: URL(string: "https://media.guim.co.uk/96d451be0fae697eb5900316a5f899a27be9486a/746_631_3523_2113/500.jpg"),
+        trailText: "A Sri Lankan twist on the classic piña colada – walks in the rain optional",
+        byline: "Sam Jones",
+        body: "This Sri Lankan take on the piña colada provides the perfect contrast to the island’s fiery curries and is a real refresher at the end of a sunny day. The Sri Lankan twist comes from Ceylon arrack, a white spirit made from the sap of the coconut flower. It’s increasingly available in the UK now, but if you can’t get hold of any, a youngish rum would make a more than decent stand-in. Serves 1 50ml coconut milk 50ml pineapple juice 50ml Ceylon arrack (or rum – go for a three-year-old Havana or similar) 10ml fresh lime juice 10ml sugar syrup Grated nutmeg, to garnish Put all the liquids in a blender, add a good handful of crushed ice and blitz to combine, until there’s a decent amount of froth on top. Pour slowly into a highball glass and serve with a dusting of nutmeg. • Sam Jones, Paradise, London W1."
+    ),
+    Recipe(
+        id: "food/2020/may/23/cocktail-recipes-clover-club-horchata-punch-sherry-richard-godwin",
+        firstPublicationDate: Date(),
+        headline: "Cocktail recipes to get your summer party started",
+        thumbnail: URL(string: "https://media.guim.co.uk/5980124d179a3c23e5fb7b5ff7ecb1e07ac1cbcd/0_247_5852_3511/500.jpg"),
+        trailText: "A twist on four classics – a gin sour, an iced tea rum punch, a sherry mixer and a punchy cinnamon cooler",
+        byline: "Richard Godwin",
+        body: "The Clover Club (pictured above) The Clover Club is a vintage cocktail that is not as well known as it should be – perhaps because no two recipes are precisely the same. I have always preferred the version listed in Cocktails: How to Mix Them, by Robert Vermeire (1922), which is a gin sour taken to filigree heights with the addition of raspberry syrup, dry vermouth and egg white. The original recipe calls for raspberry syrup, but assuming actual raspberries are available, you can simply lob two or three raspberries into the shaker with a basic sugar syrup. The vermouth is optional, but it does lend a heaven-scented herbal note to the cocktail. And the aquafaba (the water from a tin of chickpeas) functions as a waste-not-want-not vegan alternative to egg white that works just as well to create the delectable raspberry foam that sits on top of the drink. To make the golden sugar syrup for this and other recipes, pour two cups of golden caster sugar and one cup of hot water into a saucepan and stir over a gentle heat, until fully dissolved. Leave to cool, then decant. It will store in the fridge for at least a month and can be used widely in cocktails and mixed drinks. Serves 1 50ml gin 15ml fresh lemon juice (lime will work, too) 10ml golden sugar syrup 3 raspberries, plus 1 extra, to garnish 15ml French dry vermouth (eg, Dolin Dry) 15ml aquafaba (or 1 egg white) Freeze a cocktail coupe before you begin. Put all the ingredients in a cocktail shaker and fill halfway with ice. Shake hard until your fingers go numb. Strain into a spare vessel through a tea-strainer or sieve, to catch the raspberry seeds and ice shards, empty the shaker of gunk, then return the mix to the shaker and shake again for a few seconds – this second shake provides maximum froth. Pour into the frozen coupe and garnish with a fresh raspberry."
+    ),
+    Recipe(
+        id: "food/2020/dec/18/cocktail-of-the-week-cafe-baos-spiced-plum-sour-recipe",
+        firstPublicationDate: Date(),
+        headline: "Cocktail of the week: Cafe Bao's spiced plum sour – recipe",
+        thumbnail: URL(string: "https://media.guim.co.uk/383f5e7558356ad1ab380d917920a4a8ade4d531/385_1291_5105_3063/500.jpg"),
+        trailText: "A festive twist on the classic plum sour, with green plum sake, miso syrup and a bouquet of spices",
+        byline: "Cameron Malik Flynn",
+        body: "A nifty little twist on the classic plum sour, featuring subtle plum notes from the umeshu sake, a hint of festive spices from the infused gin and a rich, umami undertone from the white miso syrup. Spiced plum sour For the spiced gin 175ml gin – we use Suntory’s Roku gin 1 cinnamon stick ¼ star anise For the miso syrup 15g white miso 100g caster sugar 50ml boiling water For the drink 20ml infused gin (see above and method) 15ml miso syrup (see above and method) 25ml fresh lemon juice 20ml umeshu sake 5ml (1 tsp) blue curaçao 1 egg white (or 20ml aqua faba) First, infuse the gin. Put the gin in a sterilised jar, add the cinnamon and star anise, seal and leave to steep for up to 24 hours. Start tasting every hour or so after three or four hours, to check how much stronger you want the spices to show through – you need only a very gentle infusion here. Fine-strain to remove any tiny, loose bits of spice, jar again and seal. To make the miso syrup, put the white miso in a bowl with the caster sugar, add the boiling water and whisk until the sugar dissolves. To build the drink, put the measures of spiced gin, syrup, lemon juice, sake and curaçao in a shaker, add the egg white (or aqua faba) and shake hard without ice – this will help create a silky foam. Add ice, shake again, then strain into a wine glass or coupette (chilled, if you like, and we do), and serve. • Cameron Malik Flynn, Cafe Bao, London N1"
+    )
+]
---
+++ TheCocktail/Recipe.swift
@@ -0,0 +1,20 @@
+//
+//  Recipe.swift
+//  TheCocktail
+//
+//  Created by Krzysztof Jankowski on 24/08/2021.
+//
+
+import Foundation
+
+struct Recipe {
+
+    let id: String
+    let firstPublicationDate: Date?
+    let headline: String
+    let thumbnail: URL?
+    let trailText: String
+    let byline: String
+    let body: String
+}
+
