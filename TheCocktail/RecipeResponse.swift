91bf24e43d9e66db1399f1daec926dde75e25e83
jotkafomat <k-j@xl.wp.pl>
2021-08-31 15:58:51 +0100

    Add RecipeResponse Decodable type – Empty for the moment

---
+++ TheCocktail/RecipeResponse.swift
@@ -0,0 +1,12 @@
+//
+//  RecipeResponse.swift
+//  TheCocktail
+//
+//  Created by Krzysztof Jankowski on 31/08/2021.
+//
+
+import Foundation
+
+struct RecipeResponse: Decodable {
+    
+}
---
+++ TheCocktailTests/RecipeResponseTests.swift
@@ -0,0 +1,21 @@
+//
+//  RecipeResponseTests.swift
+//  TheCocktailTests
+//
+//  Created by Krzysztof Jankowski on 31/08/2021.
+//
+
+import Foundation
+@testable import TheCocktail
+import XCTest
+
+class RecipeResponseTest: XCTestCase {
+    
+    func testDecodesFromJSONData() throws {
+       
+        let url = try XCTUnwrap(Bundle(for: type(of: self)).url(forResource:"recipeItems",withExtension:"json"))
+        let data = try Data(contentsOf: url)
+        
+        XCTAssertNoThrow(try JSONDecoder().decode(RecipeResponse.self, from: data))
+    }
+}
