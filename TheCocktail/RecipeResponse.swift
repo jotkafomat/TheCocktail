a646725b520914e21491f3df24972980d7ab4295
jotkafomat <k-j@xl.wp.pl>
2021-09-02 10:58:53 +0100

    Decode full response from REST API

--- TheCocktail/RecipeResponse.swift
+++ TheCocktail/RecipeResponse.swift
@@ -7,7 +7,22 @@
 
 import Foundation
 
-struct RecipeResponse: Decodable {
+struct RecipeResponse {
         
     let results: [Recipe]
+}
+
+extension RecipeResponse: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case response
+    }
+    enum ResponseKeys: String, CodingKey {
+        case results
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let responseContainer = try container.nestedContainer(keyedBy: ResponseKeys.self, forKey: .response)
+        results = try responseContainer.decode([Recipe].self, forKey: .results)
+    }
 }
--- TheCocktailTests/RecipeResponseTests.swift
+++ TheCocktailTests/RecipeResponseTests.swift
@@ -11,16 +11,9 @@ import XCTest
 
 class RecipeResponseTest: XCTestCase {
     
-    func testDecodesFromJSONData() throws {
-       
-        let url = try XCTUnwrap(Bundle(for: type(of: self)).url(forResource:"recipeItems",withExtension:"json"))
-        let data = try Data(contentsOf: url)
-        
-        XCTAssertNoThrow(try JSONDecoder().decode(RecipeResponse.self, from: data))
-    }
     
-    func testRecipeResponseHasItems() throws {
-        let url = try XCTUnwrap(Bundle(for: type(of: self)).url(forResource:"recipeItems",withExtension:"json"))
+    func testRecipeDecodeFromAPIResponse() throws {
+        let url = try XCTUnwrap(Bundle(for: type(of: self)).url(forResource:"fullResponse",withExtension:"json"))
         let data = try Data(contentsOf: url)
         
         let response = try Recipe.decoder.decode(RecipeResponse.self, from: data)
